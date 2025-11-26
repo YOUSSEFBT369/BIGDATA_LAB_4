a3e6bcffcd7a67b89ec14435f58f2db2ecb30dfb
YOUSSEFBT369 <bt.youssef.369@gmail.com>
2025-11-26 15:10:19 +0000

    TP Big Data complet - Labs HDFS, MapReduce, Kafka

---
+++ reducer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import sys
+
+current_word = None
+current_count = 0
+
+for line in sys.stdin:
+    line = line.strip()
+    parts = line.split('\t', 1)
+    if len(parts) != 2:
+        continue
+    word, count = parts
+    
+    try:
+        count = int(count)
+    except ValueError:
+        continue
+        
+    if current_word == word:
+        current_count += count
+    else:
+        if current_word:
+            print('%s\t%s' % (current_word, current_count))
+        current_count = count
+        current_word = word
+
+if current_word:
+    print('%s\t%s' % (current_word, current_count))
